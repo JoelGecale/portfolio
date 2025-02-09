6831c5dae6e5ed0c40c95c985467a5d836592290
Joel Gecale <jgecale_5@yahoo.com>
2025-02-09 15:58:21 +0800

    fix bugs on Link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,9 +6,9 @@ const Header = () => {
     return(
 <div className="container-fluid">
     <header className="d-flex flex-wrap justify-content-center py-3 mb-4 px-5 border-bottom fixed-top">
-      <a href="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
+      <Link to="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
         <span className="fs-4 logo">Joel Gecale</span>
-      </a>
+      </Link>
 
       <ul className="nav nav-pills">
         <li className="nav-item"><Link to="/" className="nav-link">Home</Link></li>
